5f18af3eeb8f910de5697f2109ae072d186a2b4d
Justin Burnham <justin@jburnham.net>
2013-04-23 12:34:27 -0700

    Log the uid of the tty for tracking sudo users.

--- snoopy.c
+++ snoopy.c
@@ -21,6 +21,7 @@
 #include "snoopy.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <dlfcn.h>
 #include <syslog.h>
@@ -51,6 +52,8 @@ static inline void snoopy_log(const char *filename, char *const argv[])
 
 	char   *ttyPath         = NULL; 
 	char    ttyPathEmpty[]  = ""; 
+	char   *ttyUID          = NULL;
+	char    ttyUIDEmpty[]   = "";
 
 	#if defined(SNOOPY_EXTERNAL_FILTER)
 		FILE   *fp;
@@ -94,8 +97,16 @@ static inline void snoopy_log(const char *filename, char *const argv[])
 	ttyPath = ttyname(0);
 	if (ttyPath == NULL) {
 		ttyPath = ttyPathEmpty;
+	} else {
+		struct stat buf;
+		if (stat(ttyPath, &buf) == 0) {
+			asprintf(&ttyUID, "%d", buf.st_uid);
+		}
 	}
 
+	if (ttyUID == NULL) {
+		ttyUID = ttyUIDEmpty;
+	}
 
 	/* Allocate memory for logString */
 	logStringSize = 0;
@@ -124,9 +135,9 @@ static inline void snoopy_log(const char *filename, char *const argv[])
 	/* Create logMessage */
 	#if defined(SNOOPY_CWD_LOGGING)
 		getCwdRet = getcwd(cwd, PATH_MAX+1);
-		sprintf(logMessage, "[uid:%d sid:%d tty:%s cwd:%s filename:%s]: %s", getuid(), getsid(0), ttyPath, cwd, filename, logString);
+		sprintf(logMessage, "[uid:%d sid:%d tty:%s ttyuid:%s cwd:%s filename:%s]: %s", getuid(), getsid(0), ttyPath, ttyUID, cwd, filename, logString);
 	#else
-		sprintf(logMessage, "[uid:%d sid:%d tty:%s filename:%s]: %s",        getuid(), getsid(0), ttyPath, filename, logString);
+		sprintf(logMessage, "[uid:%d sid:%d tty:%s ttyuid:%s filename:%s]: %s",        getuid(), getsid(0), ttyPath, ttyUID, filename, logString);
 	#endif
 
 
@@ -199,6 +210,7 @@ static inline void snoopy_log(const char *filename, char *const argv[])
 	}
 
 
+	free(ttyUID);
 	/* Free the logString memory */
 	free(logString);
 	free(logMessage);
