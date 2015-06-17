c0eac909e68feb8378b66ca85f6ad4cca5b51a86
Bostjan Skufca <bostjan@a2o.si>
2015-06-17 12:35:38 +0200

    misc: remove unrequired includes from src/error.c

--- src/error.c
+++ src/error.c
@@ -31,13 +31,6 @@
 #include "configuration.h"
 #include "log.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <syslog.h>
-#include <unistd.h>
-
 
 
 /*
