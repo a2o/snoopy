fe03b4715385d98e35d33aa6c28ee413435f27a0
Bostjan Skufca <bostjan@a2o.si>
2015-06-17 23:13:57 +0000

    Build: fix get-version.sh script - add support for mode
    Release script requires getting version from changelog.
    Git build requires from git.
    
    This is a (bit awkward) solution to these requirements.

--- build/get-version.sh
+++ build/get-version.sh
@@ -24,6 +24,26 @@ set -u
 
 
 
+### Check arguments
+#
+MODE=${1:-all}
+case $MODE in
+
+    'all')
+        ;;
+    'git')
+        ;;
+    'changelog')
+        ;;
+
+    *)
+        echo "ERROR: Invalid mode: $MODE"
+        exit 1
+        ;;
+esac
+
+
+
 #### First get current commit
 ##
 #CURRENT_COMMIT_HASH=`git rev-parse HEAD`
@@ -52,10 +72,12 @@ set -u
 #
 # Used for building from git, and for making RC packages
 #
-if [ -d .git ]; then
-    CURRENT_PACKAGE_VERSION=`git describe --tags --dirty | sed -e 's/^snoopy-//'`
-    echo $CURRENT_PACKAGE_VERSION
-    exit 0
+if [ "$MODE" == "all" -o "$MODE" == "git" ]; then
+    if [ -d .git ]; then
+        CURRENT_PACKAGE_VERSION=`git describe --tags --dirty | sed -e 's/^snoopy-//'`
+        echo $CURRENT_PACKAGE_VERSION
+        exit 0
+    fi
 fi
 
 
@@ -64,15 +86,17 @@ fi
 #
 # Used only if ./bootstrap.sh is run from distribution package, not from git
 #
-if [ -f ChangeLog ]; then
-    CURRENT_PACKAGE_VERSION=`cat ChangeLog | grep -E '^[-0-9]+ - Version [.0-9]+$' | head -n1 | awk '{print $4}'`
-    echo $CURRENT_PACKAGE_VERSION
-    exit 0
+if [ "$MODE" == "all" -o "$MODE" == "changelog" ]; then
+    if [ -f ChangeLog ]; then
+        CURRENT_PACKAGE_VERSION=`cat ChangeLog | grep -E '^[-0-9]+ - Version [.0-9]+$' | head -n1 | awk '{print $4}'`
+        echo $CURRENT_PACKAGE_VERSION
+        exit 0
+    fi
 fi
 
 
 
 ### Signal error
 #
-echo "Unable to determine Snoopy version"
+echo "Unable to determine Snoopy version (mode=$MODE)!"
 exit 1
