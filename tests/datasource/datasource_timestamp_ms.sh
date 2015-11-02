083ddba6f4cb8923c8a4aa1c514559b7ea93bda9
Alasdair Campbell <alasdair@campbelldiagnostics.co.uk>
2015-11-02 15:34:05 +0000

    test suite: Fix ms rounding for datasource timestamp_ms

--- tests/datasource/datasource_timestamp_ms.sh
+++ tests/datasource/datasource_timestamp_ms.sh
@@ -26,7 +26,7 @@ fi
 ### Evaluate
 #
 VAL_DIFF=`expr $VAL_SNOOPY_2 - $VAL_SNOOPY_1`
-if [[ "$VAL_DIFF" -gt "5" ]] && [[ "$VAL_DIFF" -lt "500" ]]; then
+if [[ "$VAL_DIFF" -ge "5" ]] && [[ "$VAL_DIFF" -lt "500" ]]; then
     snoopy_testResult_pass "$VAL_DIFF"
 else
     snoopy_testResult_fail "${VAL_SNOOPY_1}ms ${VAL_SNOOPY_2}ms"
