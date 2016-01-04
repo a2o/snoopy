e98dfe27db5fcbcf78b84e3ae9c795743aaa56a2
Bostjan Skufca <bostjan@a2o.si>
2016-01-04 01:49:13 +0000

    build/random-config-flags: refactoring, and enable thread-safety when snoopy_threads datasource is enabled

--- build/get-random-configure-flags.sh
+++ build/get-random-configure-flags.sh
@@ -31,33 +31,71 @@ all-filters
 DATASOURCES=`cat $MYDIR/../configure.ac | grep SNOOPY_CONFIGURE_DATASOURCE_ | cut -d'[' -f2 | cut -d']' -f1 | sed -e 's/^/datasource-/'`
 FILTERS=`cat     $MYDIR/../configure.ac | grep SNOOPY_CONFIGURE_FILTER_     | cut -d'[' -f2 | cut -d']' -f1 | sed -e 's/^/filter-/'`
 OUTPUTS=`cat     $MYDIR/../configure.ac | grep SNOOPY_CONFIGURE_OUTPUT_     | cut -d'[' -f2 | cut -d']' -f1 | sed -e 's/^/output-/' | grep -Ev '^output-(file|socket)$'`
+ALL_CONFIGURE_OPTS="$OTHERS $DATASOURCES $FILTERS $OUTPUTS"
 
-ALL_CONFIGURE_OPTIONS="$OTHERS $DATASOURCES $FILTERS $OUTPUTS"
+# Make associative array for this
+declare -A ALL_CONFIG_OPTS_ASSOC
+for OPT in $ALL_CONFIGURE_OPTS; do
+    ALL_CONFIG_OPTS_ASSOC[$OPT]="unspecified"
+done
 
 
 
 ### Select random set of options
 #
-SELECTED_OPTIONS=""
-for OPT in $ALL_CONFIGURE_OPTIONS; do
+for OPT in "${!ALL_CONFIG_OPTS_ASSOC[@]}"; do
     RN=$[ RANDOM % 3 ]
     case $RN in
         0)
-            SELECTED_OPTIONS="$SELECTED_OPTIONS --enable-$OPT"
+            ALL_CONFIG_OPTS_ASSOC[$OPT]="enable"
             ;;
         1)
-            SELECTED_OPTIONS="$SELECTED_OPTIONS --disable-$OPT"
+            ALL_CONFIG_OPTS_ASSOC[$OPT]="disable"
             ;;
         2)
-            SELECTED_OPTIONS="$SELECTED_OPTIONS" # Unspecified
+            ALL_CONFIG_OPTS_ASSOC[$OPT]="unspecified"
             ;;
     esac
 done
 
-# Append required settings
-SELECTED_OPTIONS="$SELECTED_OPTIONS --enable-output-file --enable-output-socket"
+
+
+### Adjust selections
+#
+# Thread safety needs to be enabled for this datasource
+#
+if [ "${ALL_CONFIG_OPTS_ASSOC["datasource-snoopy_threads"]}" == "enable" ]; then
+    ALL_CONFIG_OPTS_ASSOC["thread-safety"]="enable"
+fi
+if [[ "${ALL_CONFIG_OPTS_ASSOC["everything"]}" == "enable" ]] || [[ "${ALL_CONFIG_OPTS_ASSOC["all-datasources"]}" == "enable" ]]; then
+    if [ "${ALL_CONFIG_OPTS_ASSOC["datasource-snoopy_threads"]}" != "disable" ]; then
+        ALL_CONFIG_OPTS_ASSOC["thread-safety"]="enable"
+    fi
+fi
+if [[ "${ALL_CONFIG_OPTS_ASSOC["everything"]}" == "unspecified" ]] && [[ "${ALL_CONFIG_OPTS_ASSOC["all-datasources"]}" == "unspecified" ]]; then
+    if [ "${ALL_CONFIG_OPTS_ASSOC["datasource-snoopy_threads"]}" != "disable" ]; then
+        ALL_CONFIG_OPTS_ASSOC["thread-safety"]="enable"
+    fi
+fi
+
+
+
+### Build output
+#
+SELECTED_OPTS=""
+for OPT in "${!ALL_CONFIG_OPTS_ASSOC[@]}"; do
+    case ${ALL_CONFIG_OPTS_ASSOC[$OPT]} in
+        enable)
+            SELECTED_OPTS="$SELECTED_OPTS --enable-$OPT"
+            ;;
+        disable)
+            SELECTED_OPTS="$SELECTED_OPTS --disable-$OPT"
+            ;;
+    esac
+done
+
 
 
 ### Final output
 #
-echo $SELECTED_OPTIONS
+echo $SELECTED_OPTS
