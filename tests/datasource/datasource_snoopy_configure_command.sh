8aada5288ac4db7bc5b5d290128d98342de0bcc0
Bostjan Skufca Jese <bostjan@skufca.si>
2020-11-20 16:55:04 +0100

    GH #113: Store ./configure command used during the build and make it accessible via the snoopy_configure_command data source

---
+++ tests/datasource/datasource_snoopy_configure_command.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+### Configure shell and bootstrap
+#
+set -e
+set -u
+. `dirname $BASH_SOURCE`/_bootstrap.sh
+
+
+
+### Get data
+#
+VAL_SNOOPY=`$SNOOPY_TEST_DATASOURCE snoopy_configure_command`
+
+
+
+### Evaluate
+#
+if [[ $VAL_SNOOPY =~ \./configure ]]; then
+    snoopy_testResult_pass
+else
+    snoopy_testResult_fail "The output does not start with './configure'. Instead, we got this: $VAL_SNOOPY"
+fi
