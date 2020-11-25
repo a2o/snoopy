e5d5b4ba4c8940511be23d140afd0273b95df33c
Bostjan Skufca Jese <bostjan@skufca.si>
2020-11-25 22:57:19 +0100

    GH #119: Create a test that will check the library for leaked symbols

---
+++ tests/general/general-symbol-leaks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+### Configure shell and bootstrap
+#
+set -e
+set -u
+. `dirname $BASH_SOURCE`/_bootstrap.sh
+
+
+
+### Check for leaked global symbols
+#
+RES=`nm $SNOOPY_LIBSNOOPY_TEST_SO_PATH | grep ' T ' | grep -v ' snoopy_' | grep -Ev ' execve?$' | grep -c . | cat`
+if [ "$RES" != "0" ]; then
+    nm $SNOOPY_LIBSNOOPY_TEST_SO_PATH | grep ' T ' | grep -v ' snoopy_' | grep -Ev ' execve?$' | cat
+    snoopy_testResult_fail "The .so library is leaking global symbols that are not prefixed with 'snoopy_' prefix. Inspect with 'nm $SNOOPY_LIBSNOOPY_TEST_SO_PATH' manually."
+else
+    snoopy_testResult_pass "No leaked symbols found."
+fi
