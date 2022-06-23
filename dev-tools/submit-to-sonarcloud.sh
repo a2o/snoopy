735ba9038dc6e7e1af6c71cde476398ae08f4591
Bostjan Skufca Jese <bostjan@skufca.si>
2022-06-23 20:58:23 +0000

    ci/sonar: Stop setting the `cfamily.threads` manually
    SonarCloud provided this warning:
    
        "sonar.cfamily.threads" property was set to 4, which is greater than the number of available processors: 2.
        It is recommended to let the analyzer detect the number of threads automatically by not setting the property.
        For more information, visit the documentation page:
        https://sonarcloud.io/documentation/analysis/languages/cfamily/

--- dev-tools/submit-to-sonarcloud.sh
+++ dev-tools/submit-to-sonarcloud.sh
@@ -115,7 +115,6 @@ sonar-scanner \
   -Dsonar.projectVersion=$SONARCLOUD_TAG \
   -Dsonar.cfamily.build-wrapper-output=$BUILD_WRAPPER_OUTPUT_DIR \
   -Dsonar.cfamily.gcov.reportsPath=. \
-  -Dsonar.cfamily.threads=1 \
   -Dsonar.cfamily.cache.enabled=false \
   -Dsonar.host.url=https://sonarcloud.io
 echo "INFO: Submission tag: $SONARCLOUD_TAG (branch: $CURRENT_BRANCH_NAME)"
