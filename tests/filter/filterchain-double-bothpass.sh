159148bdfd3e921b71d706014050fad6f85c306b
Bostjan Skufca Jese <bostjan@skufca.si>
2022-11-11 00:19:56 +0000

    tests: Add testing of filter chains

---
+++ tests/filter/filterchain-double-bothpass.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+### Configure shell and bootstrap
+#
+set -e
+set -u
+. `dirname $BASH_SOURCE`/_bootstrap.sh
+
+
+
+### Get data
+#
+MY_UID=`$SNOOPY_TEST_CLI run datasource uid`
+
+if $SNOOPY_TEST_CLI run filterchain   "only_uid:$MY_UID;only_uid:$MY_UID" > /dev/null; then
+    snoopy_testResult_pass
+else
+    snoopy_testResult_fail "My UID: $MY_UID"
+fi
